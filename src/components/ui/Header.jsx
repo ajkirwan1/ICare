e067c6c681fec91f9adc3a7c6eb725a125dff39e
Adam Kirwan <ajkirwan1@gmail.com>
2024-07-22 20:55:04 +0100

    set-up

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,7 @@
+/** @format */
+
+import React from "react";
+
+export function Header() {
+  return <div>Hellowsssss</div>;
+}
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import { Header } from "components/ui/Header";
+import React from "react";
+
+export function LandingPage() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
